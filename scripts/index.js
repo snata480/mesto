e9ef595c9bcc8b7f67df1b6bff278a3ad8bf71c9
Anastasiia Sai <snata480@gmail.com>
2022-03-01 22:29:03 +0300

    templait

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,59 @@ const buttonOpenPopupEdit = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
 const buttonClosePopup = popup.querySelector('.popup__close-icon');
 
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const cards = document.querySelector('.cards__list');
+
+function addCard(element) {
+    // клонируем содержимое тега template
+    const template = document.querySelector ('.template').content.cloneNode(true);
+    // наполняем содержимым
+    template.querySelector('.element__title').textContent = element.name;
+    template.querySelector('.element__foto').src = element.link;  
+    // отображаем на странице  
+    cards.append(template);
+
+}
+
+initialCards.forEach(addCard);
+
+
+
+
+
+
+
+
+
+
+
+
 let form = document.querySelector('.popup__form');
 let nameInput = form.querySelector('.popup__input_type_name');
 let jobInput = form.querySelector('.popup__input_type_job');
