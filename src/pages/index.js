138120cbe6d3afb995dd163556efe4fa66750216
Anastasiia <snata480@gmail.com>
2022-05-19 21:42:20 +0300

    added gitignore

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import { FormValidator } from './FormValidator.js';
-import { Card } from './Card.js';
-import { Section } from './Section.js';
-import Popup from './Popup.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
-import { initialCards } from './cards.js';
+import { FormValidator } from '../components/FormValidator.js';
+import { Card } from '../components/Card.js';
+import { Section } from '../components/Section.js';
+import Popup from '../components/Popup.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
+import { initialCards } from '../scripts/cards.js';
 
 const validationConfig = {
     formSelector: '.popup__form',
